ee605f316305151920b0468308d4324e1054d132
saran <saranchowdari@gmail.com>
2024-01-18 13:04:07 -0600

    wiki

--- week3/wiki/wiki/encyclopedia/views.py
+++ week3/wiki/wiki/encyclopedia/views.py
@@ -1,9 +1,27 @@
 from django.shortcuts import render
-
+import markdown
 from . import util
 
 
+
+def convert_md_to_html(title):
+    content = util.get_entry(title)
+    markdowner = markdown.Markdown()
+
+    if content == None:
+        return None
+    else:
+        return markdowner.convert(content)
+
+
+
+
+
 def index(request):
+
+    entries = util.list_entries()
+    css_file = util.get_entry("CSS")
+
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
